1f08f224e63caecc3d82d8bb07ad4dc7488f2557
Kaue1881 <kauerafael18811881@gmail.com>
2024-02-01 17:05:33 -0300

    ultimas alteações alucinantes

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function togglebtn() {
   const html = document.documentElement
 
   // trocar light para dark mode
-  if (html.classList.contains("light-mode")) {
-    html.classList.remove("light-mode")
+  if (html.classList.contains("dark-mode")) {
+    html.classList.remove("dark-mode")
   } else {
-    html.classList.add("light-mode")
+    html.classList.add("dark-mode")
   }
   // html.classList.toggle('light-mode');
 
   // trocar imagem de avatar light/dark
-  if (html.classList.contains("light-mode")) {
+  if (html.classList.contains("dark-mode")) {
     const img = document.querySelector(".avatar img")
-    img.src = "./assets/avatar-light.png"
+    img.src = "./assets/avatar-light.jpg"
   } else {
     const img = document.querySelector(".avatar img")
-    img.src = "./assets/avatar.png"
+    img.src = "./assets/avatar.jpg"
   }
 }
