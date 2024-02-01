84ce5fdf068f2c50181b8aa80b00ad65575713cc
Kaue1881 <kauerafael18811881@gmail.com>
2024-02-01 14:56:13 -0300

    :tada: script  comentado!

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// quando apertar no switch button
 function togglebtn() {
   const html = document.documentElement
 
+  // trocar light para dark mode
   if (html.classList.contains("light-mode")) {
     html.classList.remove("light-mode")
   } else {
     html.classList.add("light-mode")
   }
+  // html.classList.toggle('light-mode');
 
+  // trocar imagem de avatar light/dark
   if (html.classList.contains("light-mode")) {
     const img = document.querySelector(".avatar img")
     img.src = "./assets/avatar-light.png"
@@ -14,6 +18,4 @@ function togglebtn() {
     const img = document.querySelector(".avatar img")
     img.src = "./assets/avatar.png"
   }
-
-  // html.classList.toggle('light-mode');
 }
